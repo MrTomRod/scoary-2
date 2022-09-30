Push to 0.0.8: - update dependencies (fix security vulnerability) - use average instead of single to create dendrogram in final_overview.py - add logo, replace favicon - fix dendrogram label order

scoary/final_overview.py
@@ -106,9 +106,8 @@ def create_final_overview(summary_df: pd.DataFrame, ns: AnalyzeTraitNamespace, i
 
     logger.info('Copying files...')
     # copy files
-    files = ['overview.html', 'trait.html', 'overview.css', 'trait.css', 'overview.js', 'trait.js']
-    copy(src=f'{ROOT_DIR}/templates/overview.html', dst=f'{ns.outdir}/overview.html')
-    copy(src=f'{ROOT_DIR}/templates/trait.html', dst=f'{ns.outdir}/trait.html')
+    for file in ['overview.html', 'trait.html']:
+        copy(src=f'{ROOT_DIR}/templates/{file}', dst=f'{ns.outdir}/{file}')
     for file in ['config.json', 'trait.js', 'trait.css', 'overview.js', 'overview.css', 'favicon.svg']:
         copy(src=f'{ROOT_DIR}/templates/{file}', dst=f'{ns.outdir}/app/{file}')
 
@@ -119,15 +118,18 @@ def create_final_overview(summary_df: pd.DataFrame, ns: AnalyzeTraitNamespace, i
         pre_jaccard = ns.traits_df[summary_df.index].astype('float').T
         pre_jaccard = ((pre_jaccard.fillna(0.5) * 2) - 1).astype('int')  # False -> -1, NAN -> 0, True -> 1
 
-        # whether class=0 and class=1 are arbitrary. Calculate both possibilities, take minimum
+        # whether class=0 or class=1 is arbitrary. Calculate both possibilities, take minimum
         d1 = cdist(pre_jaccard, pre_jaccard, metric='jaccard')
         d2 = cdist(pre_jaccard, 0 - pre_jaccard, metric='jaccard')
-        jaccard_distance = np.minimum(d1, d2)
+        jaccard_distance = np.minimum(d1, d2) * 2  # multiply by 2 to make maximal distance 1 again
 
-        jaccard_df = pd.DataFrame(
-            jaccard_distance, columns=pre_jaccard.index, index=pre_jaccard.index
+        jaccard_df = pd.DataFrame(jaccard_distance, columns=pre_jaccard.index, index=pre_jaccard.index)
+        del d1, d2, jaccard_distance
+        linkage_matrix = hierarchy.linkage(
+            squareform(jaccard_df, checks=True),
+            method='average',
+            optimal_ordering=True
         )
-        linkage_matrix = hierarchy.linkage(squareform(jaccard_df), 'single')
 
         # calculate plot proportions
         content_height = max(3., len(jaccard_df) / 6)  # height dependent on number of compounds
@@ -156,7 +158,7 @@ def create_final_overview(summary_df: pd.DataFrame, ns: AnalyzeTraitNamespace, i
         dendrogram_params = plot_dendrogram(linkage_matrix, labels=jaccard_df.columns.values, ax=ax_dendrogram)
 
         # reindex summary_df according to order in dendrogram
-        summary_df_index = dendrogram_params['ivl'][::-1]
+        summary_df_index = dendrogram_params['ivl']
         summary_df = summary_df.reindex(summary_df_index)
 
         column_defs = {

tests/test_upgma.py
@@ -6,6 +6,9 @@ from unittest import TestCase
 import numpy as np
 from biotite.sequence.phylo import upgma as _biotite_upgma
 from biotite.sequence.phylo.tree import TreeNode as BiotiteTreeNode
+
+from scipy.cluster.hierarchy import linkage, to_tree, ClusterNode
+
 from scoary.upgma import upgma as scoary_upgma
 from scoary.ScoaryTree import ScoaryTree
 
@@ -22,6 +25,25 @@ def biotite_upgma(tree: BiotiteTreeNode, labels: [str]) -> ScoaryTree:
     return convert(tree.root)
 
 
+def scipy_upgma(distances, labels: [str]):
+    """
+    scipy.cluster.hierarchy.linkage: method=’average’ is called the UPGMA algorithm!
+    """
+
+    def convert(node: ClusterNode) -> ScoaryTree:
+        """recursive function"""
+        if node.count == 1:  # is_leaf
+            return ScoaryTree(label=str(node_to_label[node.id]))
+        else:
+            return ScoaryTree(left=convert(node.left), right=convert(node.right))
+
+    node_to_label: {BiotiteTreeNode: str} = dict(enumerate(labels))
+
+    Z = linkage(distances, method='average')
+    tree = to_tree(Z, False)
+    return convert(tree)
+
+
 class Test(TestCase):
     def test_upgma(self):
         distances = np.array([
@@ -36,15 +58,19 @@ class Test(TestCase):
         _biotite_tree = _biotite_upgma(distances)
         biotite_tree = biotite_upgma(_biotite_tree, labels=labels).to_list
 
+        scipy_tree = scipy_upgma(distances, labels).to_list
+
         distances_df = pd.DataFrame(distances, columns=labels)
         scoary_tree = scoary_upgma(distances_df)
 
         print(biotite_tree)
         print(scoary_tree)
+        print(scipy_tree)
 
+        assert is_equivalent_tree(biotite_tree, scipy_tree)
         assert is_equivalent_tree(biotite_tree, scoary_tree)
 
-    def test_many(self, size=20, n_tests=1000):
+    def test_scoary(self, size=20, n_tests=1000):
         labels = [f'l{i}' for i in range(size)]
 
         n_failures = 0
@@ -62,6 +88,33 @@ class Test(TestCase):
                 print('no match:')
                 print(f'  {biotite_tree=}')
                 print(f'   {scoary_tree=}')
+                n_failures += 1
+
+        print(f'{n_failures=} out of {n_tests} tests')
+        print(n_failures / n_tests)
+        self.assertLess(n_failures / n_tests, 0.05, f'Lots of failures, wtf?')
+
+    def test_scipy(self, size=20, n_tests=1000):
+        """
+        0 % agreement!
+        Evidently, scipy's upgma implementation works rather differently!
+        """
+        labels = [f'l{i}' for i in range(size)]
+
+        n_failures = 0
+        for i in range(n_tests):
+            matrix = np.random.randint(0, 2000, size=(size, size))
+            symmetrical_matrix = (matrix + matrix.T) / 2
+
+            _biotite_tree = _biotite_upgma(symmetrical_matrix)
+            biotite_tree = biotite_upgma(_biotite_tree, labels=labels).to_list
+
+            scipy_tree = scipy_upgma(symmetrical_matrix, labels).to_list
+
+            if not is_equivalent_tree(biotite_tree, scipy_tree):
+                print('no match:')
+                print(f'  {biotite_tree=}')
+                print(f'    {scipy_tree=}')
                 n_failures += 1
 
         print(f'{n_failures=} out of {n_tests} tests')